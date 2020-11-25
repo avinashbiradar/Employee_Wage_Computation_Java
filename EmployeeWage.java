a4d895509e63e20a934e690f97ac23bec34c3c0b
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-25 21:03:29 +0530

    Added:Employee wage calculation for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,27 +6,34 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation ");
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int ratePerHour=20;
-		int empHour=0;
-		Random generaterandom = new Random();
-		int randomNumber = generaterandom.nextInt(3);
-		System.out.println("Random number generated :" + randomNumber);
-		switch(randomNumber) {
-		case 1:
+		int ratePerHour = 20;
+		int empHour = 0;
+		int NUM_WORKING_DAYS = 20;
+		
+		for (int i = 0; i <= NUM_WORKING_DAYS; i++)
+		{
+			Random generaterandom = new Random();
+			int randomNumber = generaterandom.nextInt(2);
+			System.out.println("Random number generated :" + randomNumber);
+
+			switch (randomNumber)
+			{
+			case 0:
 				System.out.println("employee is full-time");
-		 		empHour=8;
-		 		int salary = ( empHour * ratePerHour);
-				System.out.println("salary is :"+salary);
+				empHour = 8;
+				int salary = (empHour * ratePerHour);
+				System.out.println("salary is :" + salary);
 				break;
-		case 2:
+			case 1:
 				System.out.println("employee is part-time");
-		        empHour=4;
-		        int salary1 = ( empHour * ratePerHour);
-		        System.out.println("salary is :"+salary1);
-		        break;
-		default :System.out.println("employee is absent");
+				empHour = 4;
+				int salary1 = (empHour * ratePerHour);
+				System.out.println("salary is :" + salary1);
+				break;
+			default:
+				System.out.println("employee is absent");
+				return;
+			}
 		}
 	}
 }
