3620564d76beea827f1fa4218f0a031f4ab7826f
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-28 05:23:49 +0530

    Added: Code for saving total wage for each company

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,28 +1,71 @@
 import java.util.Random;
 
-class CompanyEmpWage {
+class  EmployeeWageProblem {
 
-	public final String company;
-	public final int empRatePerHour;
-	public final int numOfWorkingDays;
-	public final int maxHoursPerMonth;
+	/*
+	 * uc9 This code is to check Compute Employee Wage for multiple companies
+	 * Ability to save the Total Wage for Each Company
+	 */
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
 
-	public int totalEmpWage;
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
 
-	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	private int totalEmpWage;
+
+	public EmployeeWageProblem(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		super();
 		this.company = company;
-		this.empRatePerHour = empRatePerHour;
-		this.numOfWorkingDays = numOfWorkingDays;
-		this.maxHoursPerMonth = maxHoursPerMonth;
+		this.empRatePerHour = empRatePerHour; // 20
+		this.numOfWorkingDays = numOfWorkingDays; // 2
+		this.maxHoursPerMonth = maxHoursPerMonth; // 10
 	}
 
-	public void setTotalEmpWage(int totalEmpWage) {
-		this.totalEmpWage = totalEmpWage;
+	public void computeEmpWage() {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		Random random = new Random();
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+			totalWorkingDays++;
+
+			int empCheck = random.nextInt(3);
+			// (int) Math.floor(Math.random() * 10)%3;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("day: " + totalWorkingDays + "EmpHrs: " + empHrs);
+		}
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+		// System.out.println("total emp wage for company: "+company+"is:
+		// "+totalEmpWage);
+
 	}
 
 	@Override
 	public String toString() {
 		return "Total Emp Wage for Company: " + company + " is " + totalEmpWage;
 	}
+	public static void main(String[] args)
+	{
+		EmployeeWageProblem dMart = new EmployeeWageProblem("DMart", 20, 2, 10);
+		EmployeeWageProblem reliance = new EmployeeWageProblem("Reliance", 10, 4, 20);
 
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+
+		reliance.computeEmpWage();
+		System.out.println(reliance);
+
+	}
 }
