cd28c1a50190cd96551e33c404e4b6957a3c389d
avinashbiradar <avinashbiradar179@gmail.com>
2020-11-27 22:25:57 +0530

    Added:Compute employee wage for multiple companies

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,52 +1,28 @@
 import java.util.Random;
 
-public class EmployeeWageProblem {
+class CompanyEmpWage {
 
-		
-		/*
-		 * the Code to write a Class Method to Compute Employee Wage
-		 */
-		public void empWage() 
-		{
-		int ratePerHour = 20;
-		int empHour = 0;
-		int NUM_WORKING_DAYS = 20;
-		int MAX_HRS_IN_MONTH = 100;
-		int totalEmpHrs = 0;
-		int totalWorkingDays = 0;
-		
-		while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
-		{
-			totalWorkingDays++;
-			totalEmpHrs++;
-			Random generaterandom = new Random();
-			int randomNumber = generaterandom.nextInt(2);
-			System.out.println("Random number generated :" + randomNumber);
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
 
-			switch (randomNumber) 
-			{
-			case 0:
-				System.out.println("employee is full-time");
-				empHour = 8;
-				int salary = (empHour * ratePerHour);
-				System.out.println("salary is :" + salary);
-				break;
-			case 1:
-				System.out.println("employee is part-time");
-				empHour = 4;
-				int salary1 = (empHour * ratePerHour);
-				System.out.println("salary is :" + salary1);
-				break;
-			default:
-				System.out.println("employee is absent");
-			}
-			}
-		}
-public static void main(String[] args) 
-{
-	System.out.println("Welcome to Employee Wage Computation Program");
-	EmployeeWageProblem e = new EmployeeWageProblem();
-	e.empWage();
-}
+	public int totalEmpWage;
+
+	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: " + company + " is " + totalEmpWage;
+	}
 
 }
